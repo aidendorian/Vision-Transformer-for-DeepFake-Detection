95f4e6cf8fa1b5abf0333d66fde203e47ad2231e
aidendorian <resorttoaiden@gmail.com>
2025-09-14 11:02:41 +0000

    MSA and MLP Blocks added

--- vision_transformer.py
+++ vision_transformer.py
@@ -57,3 +57,47 @@ class PatchAndEmbed(torch.nn.Module):
         
         return x_tokens
     
+class MultiheadSelfAttention(torch.nn.Module):
+    def __init__(self,
+                 embedding_dim:int=768,
+                 num_heads:int=12,
+                 attn_dropout:float=0):
+        super().__init__()
+        
+        self.layer_norm = torch.nn.LayerNorm(normalized_shape=embedding_dim)
+        
+        self.multihead_attn = torch.nn.MultiheadAttention(embed_dim=embedding_dim,
+                                                          num_heads=num_heads,
+                                                          dropout=attn_dropout,
+                                                          batch_first=True)
+        
+    def forward(self, x):
+        x = self.layer_norm(x)
+        attn_output = self.multihead_attn(query=x,
+                                          key=x,
+                                          value=x,
+                                          need_weights=False)
+        return attn_output
+    
+class MLPBlock(torch.nn.Module):
+    def __init__(self,
+                 embedding_dim:int=768,
+                 mlp_size:int=3072,
+                 drpout:float=0.1):
+        super().__init__()
+        
+        self.layer_norm = torch.nn.LayerNorm(normalized_shape=embedding_dim)
+        
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(in_features=embedding_dim,
+                            out_features=mlp_size),
+            torch.nn.GELU(p=drpout),
+            torch.nn.Linear(in_features=mlp_size,
+                            out_features=embedding_dim),
+            torch.nn.Dropout(p=drpout)
+        )
+        
+    def forward(self, x):
+        x = self.layer_norm(x)
+        x = self.mlp(x)
+        return x
