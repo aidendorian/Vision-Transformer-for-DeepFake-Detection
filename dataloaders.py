f3890422abd5ae6bf4597bffbcafc42f2e76471b
aidendorian <resorttoaiden@gmail.com>
2025-10-19 14:22:09 +0000

    took into account the new folder structure and using ImageFolder

--- dataloaders.py
+++ dataloaders.py
@@ -4,50 +4,25 @@ from PIL import Image
 import torchvision
 
 class DeepfakeDataset(torch.utils.data.Dataset):
-    def __init__(self, phase="pretraining", transform=None):
+    def __init__(self, transform=None):
         super().__init__()
-        
-        self.phase = phase
+                
         self.transform = transform
-        
-        if phase == "pretraining":
-            self.samples = [os.path.join("data/pretraining", f)
-                            for f in os.listdir("data/pretraining")]
-
-        elif phase == "finetuning":
-            real_dir = os.path.join("data/finetuning", "real")
-            fake_dir = os.path.join("data/finetuning", "fake")
-            self.samples = []
-            
-            self.samples += [(os.path.join(real_dir, f), 0) 
-                             for f in os.listdir(real_dir)]
-
-            self.samples += [(os.path.join(fake_dir, f), 1) 
-                             for f in os.listdir(fake_dir)]
+        self.samples = [os.path.join("data/pretraining", f)
+                        for f in os.listdir("data/pretraining")]
             
     def __len__(self):
         return len(self.samples)
     
     def __getitem__(self, index):
-        if self.phase == "finetuning":
-            img_path, label = self.samples[index]
-            image = Image.open(img_path).convert("RGB")
-        
-            if self.transform:
-                image = self.transform(image)
+        img_path = self.samples[index]
+        image = Image.open(img_path).convert("RGB")
             
-            return image, label
-        
-        elif self.phase == "pretraining":
-            img_path = self.samples[index]
-            image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
             
-            if self.transform:
-                image = self.transform(image)
+        return image
             
-            return image
-            
-    
 pretrain_transform = torchvision.transforms.Compose([
     torchvision.transforms.RandomResizedCrop(224),
     torchvision.transforms.RandomHorizontalFlip(),
@@ -58,14 +33,10 @@ pretrain_transform = torchvision.transforms.Compose([
 finetune_transform = torchvision.transforms.Compose([
     torchvision.transforms.RandomResizedCrop(224),
     torchvision.transforms.RandomHorizontalFlip(),
-    torchvision.transforms. ToTensor()
+    torchvision.transforms.ToTensor()
 ])
 
-pretrain_dataset = DeepfakeDataset(transform=pretrain_transform,
-                                   phase="pretraining")
-
-finetune_dataset = DeepfakeDataset(transform=finetune_transform,
-                                   phase="finetuning")
+pretrain_dataset = DeepfakeDataset(transform=pretrain_transform)
 
 def get_dataloader(phase="pretraining",
                    batch_size=16,
@@ -73,7 +44,9 @@ def get_dataloader(phase="pretraining",
                    pin_memory=True,
                    prefetch_factor=2,
                    persistent_workers=True):
+    
     if phase == "pretraining":
+        
         return torch.utils.data.DataLoader(pretrain_dataset,
                                            batch_size=batch_size,
                                            num_workers=num_workers,
@@ -81,9 +54,38 @@ def get_dataloader(phase="pretraining",
                                            persistent_workers=persistent_workers,
                                            prefetch_factor=prefetch_factor)
     elif phase == "finetuning":
-        return torch.utils.data.DataLoader(finetune_dataset,
-                                           batch_size=batch_size,
-                                           num_workers=num_workers,
-                                           pin_memory=pin_memory,
-                                           persistent_workers=persistent_workers,
-                                           prefetch_factor=prefetch_factor)
+
+        train_dataset = torchvision.datasets.ImageFolder("data/finetuning/Train",
+                                                         transform=finetune_transform)
+        
+        val_dataset = torchvision.datasets.ImageFolder("data/finetuning/Validation",
+                                                       transform=finetune_transform)
+        
+        test_dataset = torchvision.datasets.ImageFolder("data/finetuning/Test",
+                                                        transform=finetune_transform)
+        
+        train_loader = torch.utils.data.DataLoader(train_dataset,
+                                                    batch_size=batch_size,
+                                                    num_workers=num_workers,
+                                                    pin_memory=pin_memory,
+                                                    persistent_workers=persistent_workers,
+                                                    prefetch_factor=prefetch_factor,
+                                                    shuffle=True)
+        
+        val_loader = torch.utils.data.DataLoader(val_dataset,
+                                                 batch_size=batch_size,
+                                                 num_workers=num_workers,
+                                                 pin_memory=pin_memory,
+                                                 persistent_workers=persistent_workers,
+                                                 prefetch_factor=prefetch_factor,
+                                                 shuffle=True)
+        
+        test_loader = torch.utils.data.DataLoader(test_dataset,
+                                                  batch_size=batch_size,
+                                                  num_workers=num_workers,
+                                                  pin_memory=pin_memory,
+                                                  persistent_workers=persistent_workers,
+                                                  prefetch_factor=prefetch_factor,
+                                                  shuffle=True)
+        
+        return train_loader, val_loader, test_loader
